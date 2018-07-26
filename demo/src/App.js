8c62b8378f1c7302d119ae2aad905da851b37fcc
Max <max@motovilov.com>
2018-07-26 14:45:59 -0500

    Auto-redirect old URLs to v1, new to v2

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import McComponentV1 from './components/McComponent-v1';
 import MenuV1 from './components/Menu-v1';
 
@@ -12,6 +12,10 @@ const App = () => (
   <Router>
     <div>
         <div className="pane-container">
+            <Switch>
+                <Redirect from="/:id(\d+)" to="/v1/:id" />
+                <Redirect from="/" to="/v2" exact />
+            </Switch>
             <Route path="/v1/:doc?" component={MenuV1} />
             <Route path="/v1/:doc?" component={McComponentV1} />
         </div>
