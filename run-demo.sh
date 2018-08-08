e5c63a66333e91108ab34607e12d35614aa69dc9
Max <max@motovilov.com>
2018-08-08 17:12:59 -0500

    Start/stop for run-demo.sh

--- run-demo.sh
+++ run-demo.sh
@@ -1,5 +1,23 @@
 #! /bin/bash
 
+case $1 in
+	start)
+		kill -0 $(<run-demo.pid) && {
+			ps -ef | grep run-demo
+			echo To stop: ./run-demo.sh stop
+			exit 1
+		}
+		nohup ./run-demo.sh 2>&1 >running-demo.log &
+		echo !$ >run-demo.pid
+		exit 0
+	;;
+	stop)
+		kill -HUP $(<run-demo.pid)
+		rm run-demo.pid
+		exit 0
+	;;
+esac
+
 # Data backend runs on 3020
 node data/app.js &
 
